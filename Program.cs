43da802a1fbd9200969b9cda47197460bbd0bdb3
sw031 <Admin@SW031>
2023-10-06 14:28:14 +0530

    06102023 02:30 Add User,wallet,transaction tables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//added
+var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+builder.Services.AddDbContext<FlightDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("FlightReservation")));
 
 var app = builder.Build();
 
